2fcee98136ccc4afca43def586578a1fd4027fa1
Matt Simerson <matt@tnpi.net>
2016-02-21 12:24:51 -0800

    import from haraka, add tests, add cache

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var translationTable = {};
+
+exports.cont               = 900;
+exports.stop               = 901;
+exports.deny               = 902;
+exports.denysoft           = 903;
+exports.denydisconnect     = 904;
+exports.disconnect         = 905;
+exports.ok                 = 906;
+exports.next_hook          = 907;
+exports.delay              = 908;
+exports.denysoftdisconnect = 909;
+
+exports.import = function (object) {
+    for (var k in exports) {
+        if (k === 'import') continue;
+        if (k === 'translate') continue;
+        if (exports.hasOwnProperty(k)) {
+            object[k.toUpperCase()] = exports[k];
+        }
+    }
+};
+
+exports.translate = function (value) {
+
+    // cache the translationTable object
+    if (Object.keys(translationTable).length === 0) {
+        for (var k in exports) {
+            if (typeof exports[k] === 'number') {
+                translationTable[exports[k]] = k.toUpperCase();
+            }
+        }
+    }
+
+    if (translationTable[value]) return translationTable[value];
+    return 'UNKNOWN';
+};
---
+++ test/index.js
@@ -0,0 +1,39 @@
+
+var constants = require('../index');
+
+
+exports.import = {
+    'exists as function' : function (test) {
+        test.expect(1);
+        test.equal(typeof constants.import, 'function');
+        test.done();
+    },
+    'populates an object' : function (test) {
+        test.expect(1);
+        var newObj = {};
+        constants.import(newObj);
+        test.deepEqual(newObj,
+            {
+                CONT: 900,
+                STOP: 901,
+                DENY: 902,
+                DENYSOFT: 903,
+                DENYDISCONNECT: 904,
+                DISCONNECT: 905,
+                OK: 906,
+                NEXT_HOOK: 907,
+                DELAY: 908,
+                DENYSOFTDISCONNECT: 909
+            }
+        );
+        test.done();
+    }
+};
+
+exports.translate = {
+    'converts num to str' : function (test) {
+        test.expect(1);
+        test.equal('CONT', constants.translate(900));
+        test.done();
+    },
+};
